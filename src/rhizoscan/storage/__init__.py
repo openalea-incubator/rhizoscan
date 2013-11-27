aded9e9440c65eb33046237e875e1e7ef4626411
Julien Diener <julien.diener@gmail.com>
2013-11-27 12:36:25 +0000

    start add a sftp file interface
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/branches/rhizoscan@15307 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/rhizoscan/storage/__init__.py
+++ src/rhizoscan/storage/__init__.py
@@ -266,6 +266,179 @@ class FileEntry(StorageEntry):
                 meta_head = _cPickle.load(f)
                 f.close()
         
+class SFTPEntry(StorageEntry):
+    """
+    Serialization to a sftp file storage
+    
+    A SFTPEntry object has the following public methods:
+    
+      - load():   read and return content of file
+      - save():   save data into the file
+      - exists(): return True if the file exist
+      - remove(): delete the file
+      
+    :login:
+        In order to work, the calling OS should have a SSH key defined and 
+        registered on the sftp (ssh) server. It should also have an ssh-agent
+        started which mange this key.
+        
+    :dependencies:
+        paramiko
+    
+    :Serializer:
+        By default, SFTPEntry use `PickleSerializer` (which use `cPickle`) to 
+        serialize/deserialize data. However, `load` and `save` have suitable 
+        parameter to provide alternate serializer.
+        
+        See also: `PickleSerializer`, `StorageEntry`, `FileEntry`
+    """
+    __scheme__ = ('sftp')               # url scheme managed by the FileEntry class
+    __meta_file__ = '__storage__.meta'  # name of file storing metadata 
+    
+    def __init__(self, url):
+        """
+        Create a SFTPEntry object related to `url`
+        """
+        ## `read`, `write`, `save` and `load` to have serializer arg?
+        
+        self.url = url
+        self.urlparse = _urlparse.urlparse(url)
+        self._stream = None
+
+        import paramiko
+        SSH_AUTH_SOCK  = paramiko.Agent()
+        self._sshkey = SSH_AUTH_SOCK.get_keys()[0]
+        
+        self.server = None
+
+    # manage connection
+    # -----------------
+    def connect(self):
+        """ if not connected, set self.server to the sftp connection, with suitable cwd """
+        if self.server is None:
+            from . import pysftp_fork as pysftp
+            self.server = pysftp.Connection(self.urlparse.hostname, username=self.urlparse.username, private_key=self._sshkey)
+            self.server.chdir(_os.path.split(self.urlparse.path)[0])
+        else:
+            self.server._sftp_connect()
+            
+    def disconnect(self):
+        """ close self.server and set it to None """
+        self.server.close()
+        self.server = None
+        
+    # StorageEntry API
+    # ----------------
+    def exists(self):
+        self.connect()
+        exist = _os.path.split(self.urlparse.path)[1] in self.server.listdir()
+        #self._disconnect()
+        return exist
+        
+    def load(self, serializer=None):
+        """
+        open, read and return the object entry
+         """
+        stream = open(self.url, mode='r')
+        if serializer is None:
+            serializer = self.get_metadata().get('serializer',PickleSerializer)
+        data = serializer.load(stream)
+        stream.close()
+        
+        return data
+            
+    def save(self,data, serializer=None):
+        stream = _TempFile()
+        save_meta = True
+        if serializer is None:
+            serializer = PickleSerializer
+            save_meta = False
+        serializer.dump(data, stream)
+        _assert_directory(self.url)
+        with open(self.url, mode='w') as f:
+            stream.seek(0)
+            f.write(stream.read())
+        
+        if save_meta:
+            self.set_metadata(dict(serializer=serializer)) ## what if write fails?
+            
+    def remove(self):
+        """
+        Remove the entry file (and metadata) from the file system
+        """
+        if _os.path.exists(self.url):
+            _os.remove(self.url)
+        self.rem_metadata()
+
+    # manage file entry IO on metadata file
+    def _metadata(self):
+        """ return metadata file and the key for this entry """
+        dirname, filename = _os.path.split(self.url)
+        meta_file = _os.path.join(dirname,FileEntry.__meta_file__)
+        return meta_file, filename
+    @staticmethod
+    def _read_metadata_file(meta_file):
+        if _os.path.exists(meta_file):
+            try:
+                f = open(meta_file, 'r')
+                meta_head = _cPickle.load(f)
+                f.close()
+            except EOFError:
+                print '\033]31mFileEntry: invalid content in metadata file ' + meta_file + '\033]30m'
+                meta_head = dict()
+        else:
+            meta_head = dict()
+        return meta_head
+    @staticmethod
+    def _write_metadata_file(meta_file, meta_data):
+        f = open(meta_file, 'w')
+        _cPickle.dump(meta_data, f)
+        f.close()
+        
+    def set_metadata(self, metadata, update=True):
+        """
+        Save the all (key,value) of `content` in this entry metadata
+        
+        If update, update metadata with `content`.
+        otherwise, replace the whole metadata set.
+        
+        FileEntry metadata are stored in a unique file in the same directory
+        with name given in `FileEntry.__meta_file__`
+        """
+        meta_file, entry_key = self._metadata()
+        meta_head = self._read_metadata_file(meta_file)
+        
+        if update:
+            update = metadata
+            metadata = meta_head.get(entry_key,dict())
+            metadata.update(update)
+        meta_head[entry_key] = metadata
+        
+        self._write_metadata_file(meta_file, meta_head)
+            
+    def get_metadata(self):
+        """
+        return this entry metadata (as a dict)
+        """
+        meta_file, entry_key = self._metadata()
+        meta_head = self._read_metadata_file(meta_file)
+        return meta_head.get(entry_key, dict())
+        
+            
+    def rem_metadata(self):
+        meta_file, entry_key = self._metadata()
+        if _os.path.exists(meta_file):
+            f = open(meta_file, 'r')
+            meta_head = _cPickle.load(f)
+            meta_head.pop(entry_key)
+            f.close()
+            if len(meta_head)==0:
+                _os.remove(meta_file)
+            else:
+                f = open(meta_file, 'w')
+                meta_head = _cPickle.load(f)
+                f.close()
+        
 
 class MapStorage(object):
     """

b0bd015e7678db9c9ae13c978aadf7b5fa203588
Julien Diener <julien.diener@gmail.com>
2014-01-07 12:19:15 +0000

    minor variable rename
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/branches/rhizoscan@15459 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/rhizoscan/root/dev_graph2axial.py
+++ src/rhizoscan/root/dev_graph2axial.py
@@ -216,10 +216,10 @@ def path_to_axes(graph, path, axe_selection=[('length',1),('min_tip_length',10)]
                 raise NotImplementedError('axe order selection with', method, param, 'is not implemented')
                 
         elif method=='radius':
-            from rhizoscan.stats import cluster_1d as cluster
+            from rhizoscan.stats import cluster_1d
             aRadius = _np.vectorize(lambda slist:segment.radius[slist].mean())(axe)
             aRadius[_np.isnan(aRadius)] = 0
-            main_axes = cluster(aRadius, classes=2, bins=256)
+            main_axes = cluster_1d(aRadius, classes=2, bins=256)
             aOrder[main_axes] = order
             
         elif method=='seminal':

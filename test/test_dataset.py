05f405b725f352f182426a6573dc25d6413d6bab
Julien Diener <julien.diener@gmail.com>
2014-04-12 09:07:05 +0000

    test dataset
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/branches/rhizoscan@16317 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ test/test_dataset.py
@@ -0,0 +1,24 @@
+"""
+Test for rhizoscan.root.pipeline.dataset.Dataset
+"""
+
+_test_len = 6
+def test_Dataset_constructor():
+    from rhizoscan.datastructure  import Mapping
+    from rhizoscan.root.pipeline.dataset import Dataset
+    
+    ds = Dataset(Mapping(a=i/2,b=i/3,__key__=i) for i in range(_test_len))
+    assert len(ds)==_test_len, 'incorrect item number (%d/%d)' % (len(ds),_test_len)
+    return ds
+
+def test_Dataset_keys():
+    ds = test_Dataset_constructor()
+    keys = range(_test_len)
+    assert ds.keys()==keys, 'incorrect Dataset keys: '+str(ds.keys())+' instead of '+str(keys)
+
+def test_Dataset_group_by():
+    ds  = test_Dataset_constructor()
+    cds = ds.group_by('a')
+    assert cds.keys()==range(3), 'failed single key group_by'
+    cds = ds.group_by(['a','b'])
+    assert cds.keys()==[(1, 0), (0, 0), (1, 1), (2, 1)], 'failed multi-key group_by'

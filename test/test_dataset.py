ad8de64a47af9e05c3df330911e9e57ad1209c3d
Julien Diener <julien.diener@gmail.com>
2014-04-14 15:27:26 +0000

    dataset.get_column test
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/branches/rhizoscan@16348 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -7,7 +7,7 @@ def test_Dataset_constructor():
     from rhizoscan.datastructure  import Mapping
     from rhizoscan.root.pipeline.dataset import Dataset
     
-    ds = Dataset(Mapping(a=i/2,b=i/3,__key__=i) for i in range(_test_len))
+    ds = Dataset(Mapping(a=i/2,b=i/3,__key__=i, sub=Mapping(item=0)) for i in range(_test_len))
     assert len(ds)==_test_len, 'incorrect item number (%d/%d)' % (len(ds),_test_len)
     return ds
 
@@ -22,3 +22,10 @@ def test_Dataset_group_by():
     assert cds.keys()==range(3), 'failed single key group_by'
     cds = ds.group_by(['a','b'])
     assert cds.keys()==[(1, 0), (0, 0), (1, 1), (2, 1)], 'failed multi-key group_by'
+    
+def test_Dataset_get_column():
+    ds  = test_Dataset_constructor()
+    col = ds.get_column('__key__')
+    assert col==range(len(ds)), 'failed single key get_column'
+    col = ds.get_column('sub.item')
+    assert col==[0]*len(ds), 'failed multi-key get_column'

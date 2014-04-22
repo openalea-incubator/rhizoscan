76c095e990aee363a8b595a8b2f07720ccb42a13
Julien Diener <julien.diener@gmail.com>
2014-04-22 13:17:36 +0000

    branching color in editor
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/branches/rhizoscan@16409 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/rhizoscan/root/editor/tree.py
+++ src/rhizoscan/root/editor/tree.py
@@ -24,10 +24,32 @@ class RootModel(_PASModel):
         self._color_fct.append(('axe order',self.order_color))
         self.next_color('axe order')
 
+    def order(self, axe):
+        """ return the order of `axe` """
+        o = self.mtg.property('order').get(axe,None)
+        if o is None:
+            p = self.mtg.parent(axe)
+            if p: o =  self.order(p)+1
+        
+        return o if o else 0
         
     def order_color(self, segment):
         """ return the color associated to `segment` axe order """ 
-        axe = self.get_axe(segment)
-        return self.mtg.property('order').get(axe,0)
+        return self.order(self.get_axe(segment))
         
+    def _add_branching(self, segment, position):
+        """ add a branching vertex (i.e. edge_type '+') to `segment` 
+
+        And add an axe accordingly, with order = parent order +1
+        
+        return 
+          - the id of the created segment
+          - the set of updated segment
+        """
+        new_v, up = _PASModel.add_branching(self, segment=segment, position=position)
+        paxe = self.get_axe(segment)
+        saxe = self.get_axe(new_v)
+        print 'RootMode add branch', paxe, saxe
+        self.mtg.property('order')[saxe] = self.order(paxe)+1
+        return new_v, up
 

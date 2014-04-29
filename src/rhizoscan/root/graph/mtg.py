cbb0ed4f2895fe8051e8091e467ae38c7cac2f5c
Julien Diener <julien.diener@gmail.com>
2014-04-29 11:45:04 +0000

    debug on tree_to_mtg: removed seed stuff
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/branches/rhizoscan@16473 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/rhizoscan/root/graph/mtg.py
+++ src/rhizoscan/root/graph/mtg.py
@@ -14,54 +14,23 @@ from rhizoscan.workflow import node as _node # to declare workflow nodes
 @_node('mtg')
 def tree_to_mtg(tree):
     """ create a mtg from this axial tree """
-    
-    # initialization
-    # ==============
-    # node sequence of each axe, node coordinates, ...
-    node_list = tree.axe.get_node_list()[0]
-    node_pos  = [(xi,yi,0) for xi,yi in tree.node.position.T]
-    max_order = tree.axe.order().max()
-    
-    # compute seed position
-    seed_mask  = tree.segment.seed>0
-    seed_label = tree.segment.seed[seed_mask]
-    seed_id    = _np.unique(seed_label)
-    
-    seed_pos   = tree.node.position[:,tree.segment.node[seed_mask]]
-    seed_pos   = seed_pos.reshape(seed_pos.shape[0],-1)
-    seed_label = (seed_label[:,None]*_np.ones((1,2),dtype=int)).ravel()
-    x = _nd.mean(seed_pos[0], labels=seed_label, index=seed_id)
-    y = _nd.mean(seed_pos[1], labels=seed_label, index=seed_id)
-    seed_pos = _np.vstack((x,y,_np.zeros(x.size))).T
-    seed_pos = dict(zip(seed_id,seed_pos))
-
-    # create mtg tree
-    # ===============
-    #   parse axe (adding nodes) in min-order, then max-length order
-    #   1st time a node is parsed, its current axe is set as its main
-    #   hypothesis: 1st axe node (branching node) "main axe" is the parent axe
+    # - parse axe in partial order
+    # - keep mapping (plant/axe/node id in tree) to (vertex id in mtg)
+    #   for parent and complex look up
+   
     g = root_mtg()
     
+    # mapping tree-to-mtg ids
     mtg_pid = {}  # plant id in mtg - keys are tree plant id  
     mtg_aid = {}  # axe   id in mtg - keys are tree axe   id  
-    mtg_nid = {}  # node  id in mtg - keys are tree nodes id -> set the 1st processed##
+    mtg_nid = {}  # node  id in mtg - keys are (axe,nodes id)
     
     
     # add plant
     # ---------
-    for pid in seed_id:
+    for pid in set(tree.axe.plant).difference([0]):
         mtg_id = add_plant(g, plant_id=pid)
-        ##properties = dict(plant=pid)
-        ### add the plant: scale 1
-        ##g.add_component(g.root, plant=pid, label='P%d'%pid)
         mtg_pid[pid] = mtg_id
-        
-        ### add an axe (scale 2) 
-        ##v = g.add_component(mtg_id, label='G', order=0)
-        ##
-        ### add a node in its center (scale 3)
-        ##n = g.add_component(v, position=seed_pos[pid], label='g', **properties)
-        ##mtg_pid[pid] = n
         
     # for all axes (in their partial order),
     #   - add the axe to mtg
@@ -70,8 +39,8 @@ def tree_to_mtg(tree):
     axes_nodes = tree.axe.get_node_list()[0]
     tree_pos = tree.node.position
     for axe_id in tree.axe.partial_order():
-        # add the axe
-        ##parent_seg = tree.axe.parent_segment[axe_id]
+        # add axes
+        # --------
         parent_axe = tree.axe.parent[axe_id]
         axe_order  = axes_order[axe_id]
         if parent_axe==0:
@@ -82,7 +51,8 @@ def tree_to_mtg(tree):
             mtg_axe = add_axe(g, parent=mtg_aid[parent_axe], order=axe_order)
             mtg_aid[axe_id] = mtg_axe
             
-        # add the segments
+        # add segments
+        # ------------
         axe_nodes = axes_nodes[axe_id]
         node_0 = axe_nodes[0]
         if parent_axe==0:
@@ -103,79 +73,6 @@ def tree_to_mtg(tree):
             mtg_nid[(axe_id,nid)] = vid
             mtg_axe = None # next node is a successor
             
-    return g
-    
-    # To select the parent axe: 
-    #   - axe parsing follows axe asc. order, then length desc. order
-    #   - the 1st time a node is added, the current axe is set as its 'main' 
-    #     (stored mtg_nid)
-    #   - when a new axe is processed, its first node is set as its parent:
-    #      - if its first node has already been parsed: it is selected as
-    #        its parent node, which induces its parent axe
-    #      - otherwise, choose the seed node as its parent node
-    for order in range(1,max_order+1):
-        axe_mask  = tree.axe.order()==order
-        axe_order = _np.argsort(tree.axe.length()[axe_mask])[::-1]
-        
-        # add axe
-        # -------
-        g_order = g.property('order')
-        for aid in axe_mask.nonzero()[0][axe_order]:
-            # find parent node id in mtg
-            nlist  = node_list[aid]
-            if len(nlist)==0: continue
-            
-            node_0 = nlist[0]
-            properties = dict(order=order, plant=pid, axe_id=aid, radius=1)
-            
-            if not mtg_nid.has_key(node_0):  #order==1 and axes connected to seed 
-                parent_node = mtg_pid[tree.axe.plant[aid]]
-                parent_node,cur_axe = g.add_child_and_complex(parent_node, 
-                    position=node_pos[nlist[0]], 
-                    edge_type='+',
-                    **properties)
-                g.node(parent_node).label='S'
-                g.node(cur_axe).label='A'
-                g_order[cur_axe] = order
-                mtg_nid.setdefault(nlist[0],parent_node)
-                
-            else:
-                parent_node = mtg_nid[node_0]
-                # add 1st and current axe
-                position=node_pos[nlist[1]]
-                parent_node,cur_axe = g.add_child_and_complex(parent_node, 
-                        position=position,
-                        edge_type='+', **properties)
-                g.node(parent_node).label='S'
-                g.node(cur_axe).label='A'
-                g_order[cur_axe] = order
-                mtg_nid.setdefault(nlist[1],parent_node)
-                nlist = nlist[1:]
-                
-            # add nodes
-            # ---------
-            properties.pop('order')
-            for node in nlist[1:]:
-                position=node_pos[node]
-                parent_node = g.add_child(parent_node, 
-                       position=position, 
-                       x=position[0], 
-                       y=-position[2], 
-                       edge_type='<', 
-                       label='S', 
-                       **properties)
-                mtg_nid.setdefault(node,parent_node)
-                _p = g.parent(parent_node)
-                if _p is None: print '**** parent None %d ****' % parent_node
-        
-    # add edge_type to axe vertex: '+' for all axe but 1st that have no parent
-    ## second (or more) order axes which starts at seeds are not topologically second order !!!
-    edge_type = g.property('edge_type')
-    for v in g.vertices(scale=2):
-        if g.parent(v) is not None:
-            #g[v]['edge_type'] = '+' # don't work
-            edge_type[v] = '+'
-
     return g
 
 

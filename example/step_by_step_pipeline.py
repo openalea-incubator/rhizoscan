37c97ef25be3355062d5f076449fd01163d980a4
pradal <christophe.pradal@inria.fr>
2018-04-05 14:04:57 +0200

    Update gitignore and try to remove error in test

--- example/step_by_step_pipeline.py
+++ example/step_by_step_pipeline.py
@@ -5,6 +5,14 @@ from rhizoscan.root.pipeline import load_image, detect_petri_plate, compute_grap
 from rhizoscan.root.pipeline.arabidopsis import segment_image, detect_leaves
 from rhizoscan.root.graph.mtg import tree_to_mtg
 
+"""
+from path import path
+d = path('/Users/pradal/devlp/rhizoscan_images/Photos AR570')
+d = d.listdir('photos*')[0]
+fn = d.glob('*.jpg')[0]
+
+image_filename = fn
+"""
 image = load_image(image_filename)
 #plt.imshow(image);
 
@@ -12,17 +20,17 @@ pmask, px_scale, hull = detect_petri_plate(image,border_width=25, plate_size=120
 plt.imshow(pmask);
 
 rmask, bbox = segment_image(image,pmask,root_max_radius=5)
-#plt.imshow(rmask);
+plt.imshow(rmask);
 
 
 seed_map = detect_leaves(rmask, image, bbox, plant_number=2, leaf_bbox=[0,0,1,.4])
-#plt.imshow(seed_map);
+plt.imshow(seed_map);
 #plt.imshow(seed_map+rmask);
 
 graph = compute_graph(rmask,seed_map,bbox)
-#graph.plot()
+graph.plot()
 
 tree = compute_tree(graph, px_scale=px_scale)
-#tree.plot()
+tree.plot()
 
 #rsa = tree_to_mtg(tree)
--- test/test_dataset.py
+++ test/test_dataset.py
@@ -2,18 +2,18 @@
 Test for rhizoscan.root.pipeline.dataset.Dataset
 """
 
-_test_len = 6
+correct_len = 6
 def test_Dataset_constructor():
     from rhizoscan.datastructure  import Mapping
     from rhizoscan.root.pipeline.dataset import Dataset
-    
-    ds = Dataset(Mapping(a=i/2,b=i/3,__key__=i, sub=Mapping(item=0)) for i in range(_test_len))
-    assert len(ds)==_test_len, 'incorrect item number (%d/%d)' % (len(ds),_test_len)
+
+    ds = Dataset(Mapping(a=i/2,b=i/3,__key__=i, sub=Mapping(item=0)) for i in range(correct_len))
+    assert len(ds)==correct_len, 'incorrect item number (%d/%d)' % (len(ds),correct_len)
     return ds
 
 def test_Dataset_keys():
     ds = test_Dataset_constructor()
-    keys = range(_test_len)
+    keys = range(correct_len)
     assert ds.keys()==keys, 'incorrect Dataset keys: '+str(ds.keys())+' instead of '+str(keys)
 
 def test_Dataset_group_by():
@@ -22,7 +22,7 @@ def test_Dataset_group_by():
     assert cds.keys()==range(3), 'failed single key group_by'
     cds = ds.group_by(['a','b'])
     assert cds.keys()==[(1, 0), (0, 0), (1, 1), (2, 1)], 'failed multi-key group_by'
-    
+
 def test_Dataset_get_column():
     ds  = test_Dataset_constructor()
     col = ds.get_column('__key__')
